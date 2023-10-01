377315e28f3377d4d17e7dea028884713c70b5bb
Irina Shaporova <ira.shaporova@mail.ru>
2023-10-01 12:12:06 +0300

    feat: add const

---
+++ image-gallary/script.js
@@ -0,0 +1,6 @@
+const searchForm = document.getElementById ('search_form');
+const searchBox = document.getElementById ('search_box');
+const searchResult = document.getElementById ('search_result');
+const showBtn = document.getElementById ('more_btn');
+let key = "";
+let page = 1;
